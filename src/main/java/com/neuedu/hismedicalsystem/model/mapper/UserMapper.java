58fd5a720db937302017546b5c48640f4bf403e8
mvvassilev <m.v.vassilev@abv.bg>
2019-05-25 16:33:05 +0800

    user updates

---
+++ src/main/java/com/neuedu/hismedicalsystem/model/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.neuedu.hismedicalsystem.model.mapper;
+
+import com.neuedu.hismedicalsystem.model.po.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    public List<User> getUsers(User condition);
+}
---
+++ src/main/java/com/neuedu/hismedicalsystem/model/po/User.java
@@ -0,0 +1,12 @@
+package com.neuedu.hismedicalsystem.model.po;
+
+public class User {
+    private int userid;
+    private String username;
+    private String pwd;
+    private String realname;
+    private String deptcode;
+    private String usercat;
+    private String position;
+    private boolean shiftOrNot;
+}
