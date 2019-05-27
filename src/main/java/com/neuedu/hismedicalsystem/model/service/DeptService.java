af34811fcd5912f90c42c887db9dcbefd59efb86
MaxSwen <pls6666@126.com>
2019-05-27 16:25:13 +0800

    Update Delete FrontEnd working

--- src/main/java/com/neuedu/hismedicalsystem/model/service/DeptService.java
+++ src/main/java/com/neuedu/hismedicalsystem/model/service/DeptService.java
@@ -4,6 +4,7 @@ package com.neuedu.hismedicalsystem.model.service;
 import com.neuedu.hismedicalsystem.model.mapper.DeptMapper;
 import com.neuedu.hismedicalsystem.model.po.Dept;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -19,5 +20,10 @@ public class DeptService {
 
     public List<Dept> getDepts(Dept condition){
         return deptMapper.getDepts(condition);
+    }
+
+    @Transactional
+    public void addDept(Dept dept) throws Exception {
+        deptMapper.addDept(dept);
     }
 }
