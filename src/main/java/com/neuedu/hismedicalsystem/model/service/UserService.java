44633e58af9da060bc1049a975fce3bfffa33954
MaxSwen <pls6666@126.com>
2019-05-28 09:23:03 +0800

    90% Department

--- src/main/java/com/neuedu/hismedicalsystem/model/service/UserService.java
+++ src/main/java/com/neuedu/hismedicalsystem/model/service/UserService.java
@@ -4,6 +4,7 @@ package com.neuedu.hismedicalsystem.model.service;
 import com.neuedu.hismedicalsystem.model.mapper.UserMapper;
 import com.neuedu.hismedicalsystem.model.po.User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -21,5 +22,6 @@ public class UserService {
         return userMapper.getUsers(condition);
     }
 
+    @Transactional
     public void updateUser(User condition){ userMapper.updateUser(condition);}
 }
