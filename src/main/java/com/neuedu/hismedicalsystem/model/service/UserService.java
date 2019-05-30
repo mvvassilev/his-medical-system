20e838e7c6ca22345e9822041ee5885de01a9a8d
MaxSwen <pls6666@126.com>
2019-05-30 09:29:36 +0800

    Adding jasonObject dependency. Do not overwrite the whole POM.

--- src/main/java/com/neuedu/hismedicalsystem/model/service/UserService.java
+++ src/main/java/com/neuedu/hismedicalsystem/model/service/UserService.java
@@ -2,6 +2,7 @@ package com.neuedu.hismedicalsystem.model.service;
 
 
 import com.neuedu.hismedicalsystem.model.mapper.UserMapper;
+import com.neuedu.hismedicalsystem.model.po.Dept;
 import com.neuedu.hismedicalsystem.model.po.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +30,12 @@ public class UserService {
     public void deleteUser(String userid){
         userMapper.deleteUser(userid);
         userMapper.deleteUserFromUserRDept(userid);
+    }
+
+    @Transactional
+    public void addUser(User user){
+        userMapper.addUser(user);
+        List<Dept> list = user.getDepts();
+        userMapper.addUserToUserRDept(user.getUserid(), list);
     }
 }
