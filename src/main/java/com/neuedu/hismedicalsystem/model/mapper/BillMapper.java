f15314f5408b019c452b2645161d7720b6a5478c
mvvassilev <m.v.vassilev@abv.bg>
2019-06-12 09:56:08 +0800

    financial tab updates

--- src/main/java/com/neuedu/hismedicalsystem/model/mapper/BillMapper.java
+++ src/main/java/com/neuedu/hismedicalsystem/model/mapper/BillMapper.java
@@ -12,7 +12,6 @@ public interface BillMapper {
 
     List<Bill> getUnpaidBills(Bill bill);
     List<Bill> getUndoneBills(Bill bill);
-    List<Bill> getBills(Bill bill);
 
     void deleteBill(int billId);
 
